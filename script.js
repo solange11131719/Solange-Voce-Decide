8f50af04f94bbd0778e01126594417a2e9551887
solange11131719 <167382822+solange11131719@users.noreply.github.com>
2024-05-29 23:46:44 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+ const caixaPrincipal = document.querySelector(".caixa-principal");
+ const caixaPerguntas = document.querySelector(".caixa-Perguntas");
+ const caixaalternativas = document.querySelector(".caixa-alternativass");
+ const caixaresultado = document.querySelector(".caixa-resultadol");
+ const textoresultado= document.querySelector(".texto-resultdo'))
+                                              
+const perguntas =[
+  {
+  enunciado: "pergunta 1",
+  alternativas:[
+  "reposta 1",
+  "resposta 2"
+  ]  
+},
+
+    {
+    enunciado: "pergunta 1",
+    alternativas:[
+    "reposta 1",
+    "resposta 2"
+    ]  
+  },
+
+    {
+    enunciado: "pergunta 1",
+    alternativas:[
+    "reposta 1",
+    "resposta 2"
+    ]  
+  },
+
+    {
+    enunciado: "pergunta 1",
+    alternativas:[
+    "reposta 1",
+    "resposta 2"
+    ]  
+  },
+
+    {
+    enunciado: "pergunta 1",
+    alternativas:[
+    "reposta 1",
+    "resposta 2"
+    ]  
+  }
+  
+]
